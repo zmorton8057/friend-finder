107237414b2817c8b8e341fe1bb9404394f3fdc7
Zac Morton <zmorton8057@gmail.com>
2019-08-13 17:25:31 -0700

    updated survey.html and javascript to sendFile

--- server.js
+++ server.js
@@ -3,10 +3,15 @@ const app = express()
 var PORT = process.env.PORT || 3000;
 const path = require('path')
 
-// app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/app/public/home.html')))
+
 console.log(__dirname + '/app/public/home.html')
 
 app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "app/public/home.html"));
   });
+
+  app.get("/survey", function (req, res) {
+    res.sendFile(path.join(__dirname, "app/public/survey.html"));
+  });
+
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`))

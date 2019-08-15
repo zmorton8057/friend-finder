5c33f99d1e58e27c7ab922d61b0a195dfa15ccdc
Zac Morton <zmorton8057@gmail.com>
2019-08-15 13:06:58 -0700

    Included erro handling to defaul to home page id a route does not exist

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,8 +1,15 @@
 const routes = require('express').Router();
 const path = require('path')
 
-routes.get('/', (req, res) => {
+routes.get('/survey', (req, res) => {
   res.sendFile(path.join(__dirname,'../public/survey.html'))
 })
 
+routes.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname,'../public/home.html'))
+})
+///// Error handling, if a route does not exist default to the home page.
+routes.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname,'../public/home.html'))
+})
 module.exports = routes;

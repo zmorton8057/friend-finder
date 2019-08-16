e04287cda14f86061096721a516ea0e8b34ddca7
Zac Morton <zmorton8057@gmail.com>
2019-08-16 16:38:31 -0700

    Removed a console.log of the file path

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const app = require('express')();
 const routes = require("./app/routing/htmlRoutes.js");
+const data = require('./app/routing/apiRoutes.js')
 var PORT = process.env.PORT || 3000;
 
 app.use('/', routes)
 
-console.log(__dirname + '/app/public/home.html')
+app.use('/', data)
 
 app.listen(PORT, () => console.log(`Listening on Port ${PORT}!`))

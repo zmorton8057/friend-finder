74e4ddd8e777a154c0204d0fd1ab8d0f4b30b914
Zac Morton <zmorton8057@gmail.com>
2019-08-18 11:31:40 -0700

    exported json file to makin.js and receiving a respone

--- app/public/main.js
+++ app/public/main.js
@@ -1,3 +1,9 @@
-$('.sumbitScore').on('click', () => {
-    //ajax request that hits your post route in the back end
-})
+var express = require('express')
+const app = express();
+var friends = require('../data/friends')
+
+// $('.submitScore').on('click', () => {
+//     console.log(this)//ajax request that hits your post route in the back end
+// })
+
+console.log(friends[0].id)

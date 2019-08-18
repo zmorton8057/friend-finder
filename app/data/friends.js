a57b6d71f866e2e783cda332e39ca7e7567d6a5f
Zac Morton <zmorton8057@gmail.com>
2019-08-17 19:05:08 -0700

    switched from a json object to stored friends array

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,36 +1,36 @@
-{
-    [
-        {
-            "id": 1,
-            "name": "Zac",
-            "scores": [
-                5,
-                1,
-                3,
-                4,
-                4,
-                1,
-                2,
-                2,
-                2,
-                3
-            ]
-        },
-        {
-            "id": 2,
-            "name": "Victoria",
-            "scores": [
-                5,
-                1,
-                3,
-                4,
-                4,
-                1,
-                2,
-                2,
-                2,
-                3
-            ]
-        }
-    ]
-}
+var friendsArray = [
+    {
+        id: 1,
+        name: "Zac",
+        scores: [
+            5,
+            1,
+            3,
+            4,
+            4,
+            1,
+            2,
+            2,
+            2,
+            3
+        ]
+    },
+    {
+        id: 2,
+        name: "Victoria",
+        scores: [
+            5,
+            1,
+            3,
+            4,
+            4,
+            1,
+            2,
+            2,
+            2,
+            3
+        ]
+    }
+]
+
+module.exports = friendsArray;

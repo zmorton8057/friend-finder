8011a2919afc42581cad71452e34b0184644032b
Zac Morton <zmorton8057@gmail.com>
2019-08-19 01:38:53 -0700

    comparisons being made between userscore and potential matches

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -11,19 +11,39 @@ data.use(bodyParser.urlencoded({
 data.route('/friends').get(function (req, res) {
     res.json(friendsArray)
     
-});
+}); //// <============== the end of the section it needs to be placed in
+///////////////////////// This section below will need to be placed within the .get route above in order to be executed on submit. Only keeping it out for testing purposes.
+
+
+
+
+
 
 //////////// Convert the User score into its own array of scores
 var newUser = friendsArray.pop()
-console.log("NewUser:" + newUser)
 userScore = newUser.scores
-console.log(userScore)
 ////////// Converts the userScores from a string to a number
-var number = userScore.map(Number);
-console.log(number)
+var userNumber = userScore.map(Number);
+console.log("=================================")
+console.log("User Number:")
+console.log(userNumber)
+console.log("=================================")
+var holdArray = [];
+for (var i = 0; i < friendsArray.length; i++){
+    holdArray.push(friendsArray[i].scores);
+}
+console.log(holdArray)
 
 
-console.log(friendsArray)
-    
+for (var i = 0; i < holdArray.length; i++){
+  var hold = holdArray[i]
+  console.log(hold)
+  var difference = hold.map(function(item, index){
+    return item - userNumber[index]
+  })
+  console.log(difference)
+}
+
+
 
 module.exports = data;

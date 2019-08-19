ea0146c791e74a25df84982b654ef32e2e467102
Zac Morton <zmorton8057@gmail.com>
2019-08-18 23:58:47 -0700

    Converted userscore to numbers and pushed into it's own array. Removed unnecessary files

--- app/public/main.js
+++
@@ -1,9 +0,0 @@
-var express = require('express')
-const app = express();
-var friends = require('../data/friends')
-
-// $('.submitScore').on('click', () => {
-//     console.log(this)//ajax request that hits your post route in the back end
-// })
-
-console.log(friends[0].id)
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,16 +2,28 @@
 const data = require('express').Router();
 const path = require('path');
 var bodyParser = require('body-parser')
-var allFriends = require(path.join(__dirname, '../data/friends.js'))
+var friendsArray = require(path.join(__dirname, '../data/friends.js'))
 data.use(bodyParser.json());
 data.use(bodyParser.urlencoded({
   extended: true
 }));
 
 data.route('/friends').get(function (req, res) {
-    res.json(allFriends)
+    res.json(friendsArray)
+    
 });
 
+//////////// Convert the User score into its own array of scores
+var newUser = friendsArray.pop()
+console.log("NewUser:" + newUser)
+userScore = newUser.scores
+console.log(userScore)
+////////// Converts the userScores from a string to a number
+var number = userScore.map(Number);
+console.log(number)
 
+
+console.log(friendsArray)
+    
 
 module.exports = data;

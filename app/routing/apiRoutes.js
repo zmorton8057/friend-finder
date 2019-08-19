5f81793866ae6c8e202689fbef9d33b7ec3eeeee
Zac Morton <zmorton8057@gmail.com>
2019-08-18 21:38:50 -0700

    Form Data being pushed on arrays, fixed data types in friends.js to match

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,15 +2,15 @@
 const data = require('express').Router();
 const path = require('path');
 var bodyParser = require('body-parser')
+var allFriends = require(path.join(__dirname, '../data/friends.js'))
 data.use(bodyParser.json());
 data.use(bodyParser.urlencoded({
   extended: true
 }));
 
 data.route('/friends').get(function (req, res) {
-    res.sendFile(path.join(__dirname, '../data/friends.js'))
+    res.json(allFriends)
 });
-
 
 
 
--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const app = express();
 const routes = require("./app/routing/htmlRoutes.js");
 const data = require('./app/routing/apiRoutes.js');
 var bodyParser = require('body-parser')
+const path = require('path');
+var allFriends = require(path.join(__dirname, './app/data/friends.js'))
+
 
 var PORT = process.env.PORT || 3000;
 
@@ -12,6 +15,15 @@ app.use(express.json(data));
 app.use('/api', data)
 
 app.use('/', routes)
+
+app.post("/api/friends", function (req, res) {
+    allFriends.push(req.body);
+    res.json();
+});
+
+
+
+
 
 
 app.listen(PORT, () => console.log(`Listening on Port ${PORT}!`))

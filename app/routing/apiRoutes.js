e6855c9dba35d7f9fc80d91780ffbc9cf08523e0
Zac Morton <zmorton8057@gmail.com>
2019-08-19 02:16:10 -0700

    Found the index of all the differences to find friend that is matched to you

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -29,21 +29,36 @@ console.log("User Number:")
 console.log(userNumber)
 console.log("=================================")
 var holdArray = [];
+//////////////pushes the friendsArray into individual arrays only containing the scores
 for (var i = 0; i < friendsArray.length; i++){
     holdArray.push(friendsArray[i].scores);
 }
 console.log(holdArray)
 
-
+var dif = [];
 for (var i = 0; i < holdArray.length; i++){
   var hold = holdArray[i]
-  console.log(hold)
+///////////////// finds the difference between each array at each index
   var difference = hold.map(function(item, index){
     return item - userNumber[index]
   })
-  console.log(difference)
+////////// Adds together all indexes of individual differences
+  dif.push(difference.reduce((a, b) => a + b))
 }
+console.log(dif)
 
+//////////// returns the closest number in an array to 0
+function calculateClosestTo (arr) {
+  return arr.reduce((acc, x) =>
+      acc === 0 ? x :
+      x > 0 && x <= Math.abs(acc) ? x :
+      x < 0 && -x < Math.abs(acc) ? x : acc
+  , 0)
+}
+var closestToZero =calculateClosestTo(dif);
+console.log(closestToZero)
+var index = dif.indexOf(closestToZero)
+console.log(index)
 
 
 module.exports = data;

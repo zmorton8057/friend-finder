169221de1a4b84e816f98945c004cae235fb790c
Zac Morton <zmorton8057@gmail.com>
2019-08-21 21:00:52 -0700

    Updated with .then but it is causing an error

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,6 +2,7 @@
 const data = require('express').Router();
 const path = require('path');
 var bodyParser = require('body-parser')
+var $ = require("jquery");
 var friendsArray = require(path.join(__dirname, '../data/friends.js'))
 data.use(bodyParser.json());
 data.use(bodyParser.urlencoded({
@@ -9,8 +10,9 @@ data.use(bodyParser.urlencoded({
 }));
 
 data.route('/friends').get(function (req, res) {
-    res.json(friendsArray)
-    getMatch()
+  res.json(friendsArray)
+  var resp = getMatch()
+  res.send(resp)
 }); //// <============== the end of the section it needs to be placed in
 ///////////////////////// This section below will need to be placed within the .get route above in order to be executed on submit. Only keeping it out for testing purposes.
 
@@ -18,53 +20,54 @@ data.route('/friends').get(function (req, res) {
 
 
 
-function getMatch (){
+function getMatch() {
 
 
-//////////// Convert the User score into its own array of scores
-var newUser = friendsArray.pop()
-userScore = newUser.scores
-////////// Converts the userScores from a string to a number
-var userNumber = userScore.map(Number);
-console.log("=================================")
-console.log("User Number:")
-console.log(userNumber)
-console.log("=================================")
-var holdArray = [];
-//////////////pushes the friendsArray into individual arrays only containing the scores
-for (var i = 0; i < friendsArray.length; i++){
+  //////////// Convert the User score into its own array of scores
+  var newUser = friendsArray.pop()
+  userScore = newUser.scores
+  ////////// Converts the userScores from a string to a number
+  var userNumber = userScore.map(Number);
+  console.log("=================================")
+  console.log("User Number:")
+  console.log(userNumber)
+  console.log("=================================")
+  var holdArray = [];
+  //////////////pushes the friendsArray into individual arrays only containing the scores
+  for (var i = 0; i < friendsArray.length; i++) {
     holdArray.push(friendsArray[i].scores);
-}
-console.log(holdArray)
+  }
+  console.log(holdArray)
 
-var dif = [];
-for (var i = 0; i < holdArray.length; i++){
-  var hold = holdArray[i]
-///////////////// finds the difference between each array at each index
-  var difference = hold.map(function(item, index){
-    return item - userNumber[index]
-  })
-////////// Adds together all indexes of individual differences
-  dif.push(difference.reduce((a, b) => a + b))
-}
-console.log(dif)
+  var dif = [];
+  for (var i = 0; i < holdArray.length; i++) {
+    var hold = holdArray[i]
+    ///////////////// finds the difference between each array at each index
+    var difference = hold.map(function (item, index) {
+      return item - userNumber[index]
+    })
+    ////////// Adds together all indexes of individual differences
+    dif.push(difference.reduce((a, b) => a + b))
+  }
+  console.log(dif)
 
-//////////// returns the closest number in an array to 0
-function calculateClosestTo (arr) {
-  return arr.reduce((acc, x) =>
+  //////////// returns the closest number in an array to 0
+  function calculateClosestTo(arr) {
+    return arr.reduce((acc, x) =>
       acc === 0 ? x :
-      x >= 0 && x <= Math.abs(acc) ? x :
-      x <= 0 && -x <= Math.abs(acc) ? x : acc
-  , 0)
-}
-var closestToZero = calculateClosestTo(dif);
-console.log(closestToZero)
-var index = dif.indexOf(closestToZero)
-console.log(index)
-//////////// Finds the Match
-console.log(friendsArray[index])
+        x > 0 && x < Math.abs(acc) ? x :
+          x < 0 && -x < Math.abs(acc) ? x : acc
+      , 0)
+  }
+  var closestToZero = calculateClosestTo(dif);
+  console.log(closestToZero)
+  var index = dif.indexOf(closestToZero)
+  console.log(index)
+  //////////// Finds the Match
 
-
+  var match = friendsArray[index]
+  console.log(match)
+  return match
 }
 
 module.exports = data;
